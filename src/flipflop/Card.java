6da53137fb599a169ebe1fc3eb6e0157b6bc56e8
7646917 <7646917@student.swin.edu.au>
2016-10-07 20:06:00 +1100

    added pikachu

--- src/flipflop/Card.java
+++ src/flipflop/Card.java
@@ -5,22 +5,46 @@
  */
 package flipflop;
 
+import javafx.scene.image.Image;
+
 /**
  *
  * @author Daniel
  */
 public class Card {
     
-    private String name;
-    private String frontImg;
-    private String backImg;
+    private final String name;
+    private final Image frontImg;
+    private final Image backImg;
+    private String successSound;
     private int posX;
     private int posY;
+    private Image currentImg;
     
-    public Card(String name, String frontImg){
+    
+    public Card(String name, Image frontImg, int posX, int posY){
         this.name = name;
         this.frontImg = frontImg;
-        this.backImg = "cardback.jpg"
+        this.backImg = new Image("cardback.jpg",true);
+        this.posX = posX;
+        this.posY = posY;
+        this.currentImg = this.backImg;
     }
     
+    
+    public void flip(){
+        if(currentImg == backImg){
+            currentImg = frontImg;
+        }else{
+            currentImg = backImg;
+        }
+        
+    }
+    
+    public Image getCurrentImg(){
+        return this.currentImg;
+    }
+    public Image getBackImg(){
+        return this.backImg;
+    }
 }
--- src/flipflop/FlipFlop.java
+++ src/flipflop/FlipFlop.java
@@ -11,6 +11,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -19,28 +23,57 @@ import javafx.stage.Stage;
  * @author Daniel
  */
 public class FlipFlop extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
+        //CREATE ALL CARDS;
         ArrayList<Card> cards = new ArrayList<>();
-        
-        cards.add(new Card("Pikachu","pikachu.jpg"));
-        
+        Image pikachu = new Image("pikachu.jpg");
+        Card pika = new Card("Pikachu", pikachu, 0, 0);
+        cards.add(pika);
         Button btn = new Button();
         btn.setText("Start Game");
+        StackPane root = new StackPane();
+
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Started game!");
+                ImageView iv1 = new ImageView();
+                iv1.setImage(pika.getCurrentImg());
+                iv1.setFitWidth(150);
+                iv1.setPreserveRatio(true);
+                iv1.setSmooth(true);
+                iv1.setCache(true);
+
+                iv1.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+                    @Override
+                    public void handle(MouseEvent event) {
+                        // Check all cards, if match, leave both clicked. //card1 card2 etc.
+                        //else flip them again
+                        pika.flip();
+                        System.out.println("flip");
+                        iv1.setImage(pika.getCurrentImg());
+                        event.consume();
+                    }
+                });
+
+                HBox box = new HBox();
+                box.getChildren().add(iv1);
+                root.getChildren().add(box);
+
+                //primaryStage.setScene(scene2);
+                primaryStage.show();
+
             }
         });
-        
-        StackPane root = new StackPane();
+
         root.getChildren().add(btn);
-        
+
         Scene scene = new Scene(root, 300, 250);
-        
+
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -52,5 +85,5 @@ public class FlipFlop extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
